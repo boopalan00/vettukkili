dcae55b06c2262f2db07d0e5bdacf96fa3b6df91
paymaram <boopalan28012003@gmail.com>
2023-07-11 21:37:07 +0530

    update

--- gh.py
+++ gh.py
@@ -1,5 +1,17 @@
 import json
+from argparse import ArgumentParser 
 
+parse = ArgumentParser(description="*", epilog="----------")
+
+
+parse.add_argument()
+parse.add_argument()
+parse.add_argument()
+parse.add_argument()
+
+
+
+ags = parse.parse_args()
 
 
 
